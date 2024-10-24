1ef2b86bc368fe245c66734b747562a9ec3f33a0
samuel <samiehave9@gmail.com>
2024-10-24 12:50:55 +0300

    capitalize the first letter of the word

--- ruby_basics/1_data_types/exercises/string_exercises.rb
+++ ruby_basics/1_data_types/exercises/string_exercises.rb
@@ -34,12 +34,12 @@ def substrings(word)
 end
 substrings("substring")
 
-# def capitalize(word)
-#   # capitalize the first letter of the word
-#   word = "capitalize"
-#   puts word.capitalize
-# end
-# capitalize("capitalize")
+def capitalize(word)
+  # capitalize the first letter of the word
+  word = "capitalize"
+  puts word.capitalize
+end
+capitalize("capitalize")
 
 # def uppercase(string)
 #   # uppercase all letters in the string
