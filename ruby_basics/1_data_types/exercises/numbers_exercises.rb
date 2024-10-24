1d23c1070c34818dc6140100b7e3cecf01107b09
samuel <samiehave9@gmail.com>
2024-10-24 11:20:04 +0300

    Odd N Even functionality

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -36,27 +36,27 @@ remainder(11, 5)
 def float_division(a, b)
   # return the result of dividing a by b as a float, rather than an integer
   float_division = a / b
-  puts "Float_divisione #{float_division}"
+  puts "Float_division: #{float_division}"
 end
 float_division(7.3, 2)
 
 def string_to_number(string)
   # return the result of converting a string into an integer
-  string_to_number = string.to_i
+  string = string.to_i
   puts "String: #{string}"
 end
 string_to_number("23")
 
-# def even?(number)
-#   # return true if the number is even (hint: use integer's even? method)
-#   puts number.even?
-# end
-# even?(4)
-# even?(3)
+def even?(number)
+  # return true if the number is even (hint: use integer's even? method)
+  puts number.even?
+end
+even?(4)
+even?(3)
 
-# def odd?(number)
-#   # return true if the number is odd (hint: use integer's odd? method)
-#   puts number.odd?
-# end
-# odd?(8)
-# odd?(1)
+def odd?(number)
+  # return true if the number is odd (hint: use integer's odd? method)
+  puts number.odd?
+end
+odd?(8)
+odd?(1)
