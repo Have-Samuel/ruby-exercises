9ba47f8d215c99fd8c662263da5e7c4193213f46
samuel <samiehave9@gmail.com>
2024-10-24 12:43:52 +0300

    Second Concatenation

--- ruby_basics/1_data_types/exercises/string_exercises.rb
+++ ruby_basics/1_data_types/exercises/string_exercises.rb
@@ -11,14 +11,16 @@ def concatenate_example(string)
 end
 concatenate_example("string")
 
-# def concatenate(string)
-#   # use concatenation to format the result to be "Hello <string>!"
-#   greeting = "Hello"
-#   str = "string"
-#   sayHi = greeting << " " << str << "!"
-#   puts sayHi
-# end
-# concatenate("string")
+def concatenate(string)
+  # use concatenation to format the result to be "Hello <string>!"
+  greeting = "Hello"
+  str = "string"
+  # sayHi = greeting << " " << str << "!"
+  # puts sayHi
+  sayIt = "#{greeting} #{str}!"
+  puts sayIt
+end
+concatenate("string")
 
 # def substrings(word)
 #   # return the first 4 letters from the word using substrings
